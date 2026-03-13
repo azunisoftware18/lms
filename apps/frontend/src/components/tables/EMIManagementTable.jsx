f1840c8df0dd988b46310b468e983d11728ca761
mdmaj <mazidhussain.md23237@jeckukas.org.in>
2026-03-13 12:38:51 +0530

    feat: Replace table implementation in EMIManagementPage with EMIManagementTable component

---
+++ apps/frontend/src/components/tables/EMIManagementTable.jsx
@@ -0,0 +1,42 @@
+import Pagination from "../common/Pagination";
+import { TableShell, TableHead, TableBody } from "./core";
+
+export default function EMIManagementTable({
+  title = "",
+  columns = [],
+  data = [],
+  actions = [],
+  search = "",
+  onSearchChange = () => {},
+  filterValue = "",
+  onFilterChange = () => {},
+  filterOptions = [],
+  currentPage = 1,
+  totalPages = 1,
+  onPageChange = () => {},
+}) {
+  return (
+    <>
+      <TableShell>
+        <TableHead
+          title={title}
+          columns={columns}
+          search={search}
+          setSearch={onSearchChange}
+          filterValue={filterValue}
+          setFilterValue={onFilterChange}
+          filterOptions={filterOptions}
+        />
+        <TableBody columns={columns} data={data} actions={actions} />
+      </TableShell>
+
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={onPageChange}
+        />
+      )}
+    </>
+  );
+}
